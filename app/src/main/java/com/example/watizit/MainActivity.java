e17a3f9342d29cc994eb33bdf7ab05b059a4509e
Rooly <kaizokuoni974@gmail.com>
2020-02-07 10:29:42 +0400

    Added function to switch between pages

--- app/src/main/java/com/example/watizit/MainActivity.java
+++ app/src/main/java/com/example/watizit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.watizit;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void option(View view){
+        startActivity(new Intent(this, menu_options.class));
     }
 }
---
+++ app/src/main/java/com/example/watizit/menu_options.java
@@ -0,0 +1,11 @@
+package com.example.watizit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menu_options extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_options);
+    }
+}
