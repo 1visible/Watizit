79b497fb265f04d623e87fe5621f5ebed4e93048
Rooly <kaizokuoni974@gmail.com>
2020-02-08 13:23:13 +0400

    Added clickable link, beginning "jouer" menu

--- app/src/main/java/com/example/watizit/MainActivity.java
+++ app/src/main/java/com/example/watizit/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.watizit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
 
     @Override
--- app/src/main/java/com/example/watizit/MenuOptions.java
+++ app/src/main/java/com/example/watizit/MenuOptions.java
@@ -1,11 +1,18 @@
 package com.example.watizit;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuOptions extends AppCompatActivity {
+    TextView textView;
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_options);
+        textView = findViewById(R.id.copyright);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
