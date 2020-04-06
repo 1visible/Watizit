729097bbd79196fa38377e12e09a99a69034f3f8
Rooly <kaizokuoni974@gmail.com>
2020-04-06 22:02:33 +0400

    Add music in background

--- app/src/main/java/com/example/watizit/menus/MainActivity.java
+++ app/src/main/java/com/example/watizit/menus/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.watizit.menus;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.watizit.R;
+import com.example.watizit.utils.HomeWatcher;
+import com.example.watizit.utils.JouerMusique;
 import com.example.watizit.utils.WatizUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeWatcher mHomeWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,28 @@ public class MainActivity extends AppCompatActivity {
         WatizUtil.setBackgroundColor(this, bouton_jouer, R.color.COLOR_BLUE);
         WatizUtil.setBackgroundColor(this, bouton_options, R.color.COLOR_GRAY);
 
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, JouerMusique.class);
+        startService(music);
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
+
     }
 
     public void option(View view){
@@ -41,6 +72,75 @@ public class MainActivity extends AppCompatActivity {
 
     public void jouer(View view){
         startActivity(new Intent(this, MenuJouer.class));
+    }
+
+    private boolean mIsBound = false;
+    private JouerMusique mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((JouerMusique.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,JouerMusique.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,JouerMusique.class);
+        stopService(music);
+
     }
 
 }
